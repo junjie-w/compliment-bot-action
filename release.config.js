ad47ca13027aeca10441b46c45337b33b1bda0e1
junjie-w <junjiewu.brooke@gmail.com>
2024-11-01 11:47:41 +0100

    fix: update release config

--- release.config.js
+++ release.config.js
@@ -4,6 +4,7 @@ const config = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    '@semantic-release/npm',
     '@semantic-release/github',
     [
       '@semantic-release/git',
